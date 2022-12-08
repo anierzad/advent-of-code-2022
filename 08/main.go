6c443758fa10fd50fe5591b0bcaedae328404c86
Adam Nierzad <adam.nierzad@awaze.co.uk>
2022-12-08 12:22:35 +0000

    Solved day 08 part two.

---
+++ 08/main.go
@@ -0,0 +1,171 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+type Point struct {
+	X, Y int
+}
+
+func NewPoint(x, y int) Point {
+	return Point{
+		X: x,
+		Y: y,
+	}
+}
+
+type Tree struct {
+	Position Point
+	Height   int
+}
+
+func (t *Tree) Visible(forest Forest) bool {
+
+	// Check north.
+	visN := true
+	for y := t.Position.Y-1;; y-- {
+		p := NewPoint(t.Position.X, y)
+
+		ct, exists := forest[p]
+		if exists {
+
+			if ct.Height >= t.Height {
+				visN = false
+				break
+			}
+			continue
+		}
+		break
+	}
+
+	// Check east.
+	visE := true
+	for x := t.Position.X+1;; x++ {
+		p := NewPoint(x, t.Position.Y)
+
+		ct, exists := forest[p]
+		if exists {
+
+			if ct.Height >= t.Height {
+				visE = false
+				break
+			}
+			continue
+		}
+		break
+	}
+
+	// Check south.
+	visS := true
+	for y := t.Position.Y+1;; y++ {
+		p := NewPoint(t.Position.X, y)
+
+		ct, exists := forest[p]
+		if exists {
+			if ct.Height >= t.Height {
+				visS = false
+				break
+			}
+			continue
+		}
+		break
+	}
+
+	// Check west.
+	visW := true
+	for x := t.Position.X-1;; x-- {
+		p := NewPoint(x, t.Position.Y)
+
+		ct, exists := forest[p]
+		if exists {
+			if ct.Height >= t.Height {
+				visW = false
+				break
+			}
+			continue
+		}
+		break
+	}
+
+	return visN || visE || visS || visW
+}
+
+type Forest map[Point]Tree
+
+func (f *Forest) AddTree(x, y, height int) {
+	p := NewPoint(x, y)
+	(*f)[p] = Tree{
+		Position: p,
+		Height: height,
+	}
+}
+
+func (f Forest) PrintVis() {
+	var maxX, maxY int
+	for p := range f {
+		if p.X > maxX {
+			maxX = p.X
+		}
+		if p.Y > maxY {
+			maxY = p.Y
+		}
+	}
+
+	fmt.Println()
+	for y := 0; y <= maxY; y++ {
+		for x := 0; x <= maxX; x++ {
+			p := NewPoint(x, y)
+			t := f[p]
+			v := t.Visible(f)
+			r := 'O'
+			if v {
+				r = '*'
+			}
+			fmt.Print(string(r))
+		}
+		fmt.Println()
+	}
+	fmt.Println()
+}
+
+func main() {
+
+	f, err := os.Open("./input")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer f.Close()
+
+	forest := Forest{}
+
+	scanner := bufio.NewScanner(f)
+	yPos := 0
+	for scanner.Scan() {
+		line := scanner.Text()
+		for xPos, h := range line {
+			height, err := strconv.Atoi(string(h))
+			if err != nil {
+				log.Fatal(err)
+			}
+			forest.AddTree(xPos, yPos, height)
+		}
+		yPos++
+	}
+
+	total := 0
+	for _, t := range forest {
+		if t.Visible(forest) {
+			total++
+		}
+	}
+
+	forest.PrintVis()
+
+	fmt.Println("Visible trees:", total)
+}
